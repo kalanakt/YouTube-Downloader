2c93092a1ff2e8214ac0af80794f89c4c95d62b9
kalanakt <e19198@eng.pdn.ac.lk>
2023-02-20 17:50:39 +0530

    fix error

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID"))
-    API_HASH = os.getenv("API_HASH")
-    BOT_TOKEN = os.getenv("BOT_TOKEN")
+    API_ID = int(os.getenv("API_ID", ))
+    API_HASH = os.getenv("API_HASH", '')
+    BOT_TOKEN = os.getenv("BOT_TOKEN", '')
