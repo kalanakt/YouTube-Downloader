9a26ef91b910406d6be0bade4226135a65f3aa03
kalana kithmina <kalanakithmina99@gmail.com>
2023-02-20 17:01:53 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    API_ID = int(os.environ.get("API_ID", "6129651"))
-    API_HASH = os.environ.get("API_HASH", "12c5b64ffbd1425f2cf0f2910bc16ef5")
-    BOT_TOKEN = os.environ.get(
-        "BOT_TOKEN", "6083920856:AAF1p6FXTvl3GDEDNIvOKp4Nhchon6h9sYc")
